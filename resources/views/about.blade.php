84fa9d1d8881e38bef949dbe63a5c472566b091c
Daffa <daffafebriansaputra101@gmail.com>
2024-09-02 20:47:03 +0700

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman About</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <a href="/">Home</a>
+    <a href="/blog">Blog</a>
+    <a href="/about">About</a>
+    <a href="/contact">contact</a>
+
+
+    <h1>About</h1>
+    <h3>Owner: {{ $nama }}</h3>
+    <img src="img/valorant.jpg" alt="Valo Guys" width="200">
+</body>
+
+</html>
---
+++ resources/views/blog.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog E-Sport</title>
+</head>
+
+<body>
+    <a href="/">Home</a>
+    <a href="/blog">Blog</a>\
+    <a href="/about">About</a>
+    <a href="/contact">contact</a>
+
+
+    <h1>Paper Rex</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda iste iusto quam vel optio facere distinctio!
+        Delectus exercitationem ad nobis voluptas similique eaque omnis vel quas autem? Rem, asperiores dicta?</p>
+
+    <h1>EDG</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab ad quibusdam porro iste reprehenderit nihil quisquam
+        quae, illo repellendus dignissimos sapiente deleniti maiores incidunt ducimus, impedit aut? Qui, laudantium
+        quod.</p>
+</body>
+
+</html>
